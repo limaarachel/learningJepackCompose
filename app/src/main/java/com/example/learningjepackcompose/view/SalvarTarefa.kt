a201067ebecf1d8f585e232de530035e2d595974
rachel lima <rachellimaa@gmail.com>
2025-05-13 18:08:31 -0300

    first commit structure

---
+++ app/src/main/java/com/example/learningjepackcompose/view/SalvarTarefa.kt
@@ -0,0 +1,12 @@
+package com.example.learningjepackcompose.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SalvarTarefa(
+    navController: NavController
+) {
+    Text(text = "Salvar tarefas")
+}
